cadf18a1a2114f15e9cf2ceeff474f7acbab57ac
Diogo Ferreira <defer@cyanogenmod.com>
2011-10-23 17:11:55 +0100

    pigz: Create an unpigz symlink
    Creates an unpigz symlink when doing a full build. Note: it won't
    work if doing something like 'mm' or 'mmm'.

--- Android.mk
+++ Android.mk
@@ -12,3 +12,19 @@ LOCAL_SHARED_LIBRARIES += libz libc
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
 include $(BUILD_EXECUTABLE)
+
+PIGZ_TOOLS := unpigz
+SYMLINKS := $(addprefix $(TARGET_OUT_OPTIONAL_EXECUTABLES)/,$(PIGZ_TOOLS))
+$(SYMLINKS): PIGZ_BINARY := $(LOCAL_MODULE)
+$(SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Symlink: $@ -> $(PIGZ_BINARY)"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf $(PIGZ_BINARY) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(SYMLINKS)
+
+# We need this so that the installed files could be picked up based on the
+# local module name
+ALL_MODULES.$(LOCAL_MODULE).INSTALLED := \
+    $(ALL_MODULES.$(LOCAL_MODULE).INSTALLED) $(SYMLINKS)
